4ec3333ab108005764403dcc5115a88451a7e5b4
nerdymedic <mgoebel@gmail.com>
2024-06-16 09:38:16 -0400

    updated for new hugo academic template

--- talk_gen.R
+++ talk_gen.R
@@ -1,63 +1,71 @@
 library(tidyverse)
 library(lubridate)
 
-#function to create markdown files
+talks <- read_csv("talk_generator/talks.csv", col_types = cols(.default = "c"))
+
+talks_formatted <- paste0("---
+title: ", talks$title, "
+
+event: ", talks$event, "
+event_url: ", talks$event_url, "
+
+location: ", talks$location, "
+address:
+  street: ", talks$street, "
+  city: ", talks$city, "
+  region: ", talks$region, "
+  postcode: ", talks$postcode, "
+  country: ", talks$country, "
+
+summary: ", talks$summary, "
+
+# Talk start and end times.
+#   End time can optionally be hidden by prefixing the line with `#`.
+date: ", mdy(talks$date), "
+#date_end: '2030-06-01T15:00:00Z'
+all_day: true
+
+# Schedule page publish date (NOT talk date).
+publishDate: '2017-01-01T00:00:00Z'
+
+authors:
+  - admin
+
+tags: []
+
+# Is this a featured talk? (true/false)
+featured: false
+
+#links:
+#  - icon: twitter
+#    icon_pack: fab
+#    name: Follow
+#    url: https://twitter.com/georgecushen
+url_code: ''
+url_pdf: ''
+url_slides: ", talks$url_slides, "
+url_video: ", talks$url_video, "
+
+---")
+
+talks_formatted <- str_replace_all(talks_formatted, "NA", "")
+
 write_md <- function(text, filename) {
   fileConn<-file(filename)
   writeLines(text, fileConn)
   close(fileConn)
 }
 
-#read in the talks
-talks <- read_csv("talks.csv", col_types = cols(.default = "c"))
-
-#generate the markdown
-talks_formatted <- paste0("---
-abstract: ", talks$abstract, "
-address:
-  city: ", talks$city, "
-  country:
-  postcode: 
-  region: ", talks$region, "
-  street: 
-all_day: true
-authors: []
-date: ", mdy(talks$date), "
-event: ", talks$event, "
-event_url: 
-featured: false
-location: ", talks$location, "
-math: true
-publishDate: ", today(),"
-summary: ", talks$summary, "
-tags: []
-title: ", talks$title, "
-url_code: ", talks$url_code,"
-url_pdf: ", talks$url_pdf,"
-url_slides: ", talks$url_slides,"
-url_video: ", talks$url_video,"
----"
-)
-
-#remove NAs
-talks_formatted <- str_replace_all(talks_formatted, "NA", "")
-
-#create list of folder  names
-foldernames <- paste0("content/talk/event", 
+foldernames <- paste0("content/event/event", 
                     seq(1:length(talks_formatted)))
 
-#create  list of filesname
-filenames <- paste0("content/talk/event", 
+filenames <- paste0("content/event/event", 
                     seq(1:length(talks_formatted)),
                     "/index.md")
 
-#remove files and folders that already exist
-#comment out to overwrite exisitng talks
-foldernames <- foldernames[map_lgl(foldernames, dir.exists) == FALSE]
-filenames <- filenames[map_lgl(filenames, file.exists) == FALSE]
+#comment out to overwrite all events
+# foldernames <- foldernames[map_lgl(foldernames, dir.exists) == FALSE]
+# filenames <- filenames[map_lgl(filenames, file.exists) == FALSE]
 
-#create the folders
 map(foldernames, dir.create)
-
-#create the markdown files
 map2(talks_formatted, filenames, ~write_md(.x, .y))
